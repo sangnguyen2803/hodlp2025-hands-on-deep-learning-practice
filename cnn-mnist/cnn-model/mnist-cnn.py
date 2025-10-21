d89392bc138ac2219143b33505021b0d6162b286
sangnguyen2803 <sangnguyen280300@gmail.com>
2025-10-21 16:28:32 +0200

    MNIST CNN - cnn model

--- cnn-mnist/cnn-model/mnist-cnn.py
+++ cnn-mnist/cnn-model/mnist-cnn.py
@@ -3,9 +3,10 @@ from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import torch
-
+from tqdm import tqdm 
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 def load_mnist_images(filename):
     with open(filename, 'rb') as f:
@@ -42,6 +43,7 @@ class MNISTDataset(Dataset):
             # convert to float32 ranging from 0.0 to 1.0, applying normalization, resizing and augmenting to prepare the image for neural network
             # transform method includes the torch.tensor()
             img = self.transform(img)
+        return img, label
 
 class CNN(nn.Module):
     def __init__(self, num_classes=10, dropout=0.5):
@@ -51,12 +53,67 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
         # Layer 2:
-        
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        # Layer 3:
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        # Fully-connected
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.dropout = nn.Dropout(dropout)
+        self.fc2 = nn.Linear(256, num_classes)
 
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+def train(model, loader, criterion, optimizer, device, epoch, total_epochs):
+    model.train()
+    running_loss = 0.0
+    progress_bar = tqdm(loader, desc=f"Epoch [{epoch}/{total_epochs}] Training", leave=False)
+    for images, labels in progress_bar:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        progress_bar.set_postfix(loss=f"{loss.item():.4f}")
+    return running_loss / len(loader)
+
+
+def evaluate(model, loader, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return 100 * correct / total
 
 def main():
-    train_images = load_mnist_images('train-images.idx3-ubytes')
-    train_labels = load_mnist_labels('train-labels.idx1-ubytes')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_images = load_mnist_images('../dataset/train-images.idx3-ubyte')
+    train_labels = load_mnist_labels('../dataset/train-labels.idx1-ubyte')
+
+    test_images = load_mnist_images('../dataset/t10k-images.idx3-ubyte')
+    test_labels = load_mnist_labels('../dataset/t10k-labels.idx1-ubyte')
+
+    print('Shape of train images: ', train_images.shape)
     transform = transforms.Compose([
         transforms.Resize((32, 32)),
         transforms.Normalize((0.5,), (0.5,)) # normalize to [-1, 1]
@@ -72,7 +129,31 @@ def main():
         batch_size=64,
         shuffle=True
     )
+    test_dataset = MNISTDataset(
+        test_images,
+        test_labels,
+        transform=transform
+    )
+    test_loader = DataLoader(
+        test_dataset, 
+        batch_size=64, 
+        shuffle=False
+    )
 
+    model = CNN(num_classes=10, dropout=0.5).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    print(f'CNN Model: {model}\n')
+    print('Training CNN Model:\n')
+
+    num_epochs = 5
+    for epoch in range(1, num_epochs + 1):
+        loss = train(model, train_loader, criterion, optimizer, device, epoch, num_epochs)
+        acc = evaluate(model, test_loader, device)
+        print(f"[Epoch {epoch}/{num_epochs}] Loss: {loss:.4f} | Test Acc: {acc:.2f}%")
+
+    # Save model
+    torch.save(model.state_dict(), "../results/mnist_cnn_manual.pth")
 
 #     args = parse_args()
 #     # choose device having GPU
